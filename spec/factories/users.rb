64d927a763b83748fcd104d483340431743e011a
Carsano <damien.fabiano@gadz.org>
2019-09-09 22:01:07 +0200

    update facrotybot rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,28 +1,28 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.email}
-    password { Faker::Internet.password}
-    first_name { Faker::Name.first_name}
-    last_name { Faker::Name.last_name}
-    phone_number { %w(0606060606 +33606060606).sample}
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    phone_number { %w[0606060606 +33606060606].sample }
 
     factory :user_empty_email do
-      email {""}
+      email { '' }
     end
     factory :user_bad_email do
-      email {"sdf"}
+      email { 'sdf' }
     end
     factory :user_empty_password do
-      password {""}
+      password { '' }
     end
     factory :user_bad_password do
-      password {"sdf"}
+      password { 'sdf' }
     end
     factory :user_empty_phone do
-      phone_number {""}
+      phone_number { '' }
     end
     factory :user_bad_phone do
-      phone_number {"0606"}
+      phone_number { '0606' }
     end
   end
 end
