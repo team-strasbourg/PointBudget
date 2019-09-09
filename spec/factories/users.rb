67f41b6796fa83c6147a7e99a4e3b9c9c27508ff
Carsano <damien.fabiano@gadz.org>
2019-09-09 21:08:47 +0200

    update factory user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email}
+    password { Faker::Internet.password}
+    first_name { Faker::Name.first_name}
+    last_name { Faker::Name.last_name}
+    phone_number { '0606060606'}
+
+    factory :user_empty_email do
+      first_name {""}
+    end
+    factory :user_empty_password do
+      first_name {""}
+    end
   end
 end
