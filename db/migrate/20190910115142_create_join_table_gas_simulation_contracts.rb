47dede40fe881adf21319c35411a9d1932f7eee5
Carsano <damien.fabiano@gadz.org>
2019-09-10 14:25:56 +0200

    add index true in migration

--- db/migrate/20190910115142_create_join_table_gas_simulation_contracts.rb
+++ db/migrate/20190910115142_create_join_table_gas_simulation_contracts.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableGasSimulationContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :join_table_gas_simulation_contracts do |t|
-      t.belongs_to :gas_simulation
-      t.belongs_to :gas_contract
+      t.belongs_to :gas_simulation, index: true
+      t.belongs_to :gas_contract, index: true
       t.timestamps
     end
   end
