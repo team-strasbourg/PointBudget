92e14b3caec93017802a53c595e6128007160165
iroshiva <jean.weil@protonmail.com>
2019-09-10 13:20:24 +0200

    add css file kit_ui in vendor + link application.css

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_10_084915) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_09_10_084915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_full_simulations_on_user_id"
-
   end
 
   create_table "gas_contracts", force: :cascade do |t|
@@ -58,7 +55,6 @@ ActiveRecord::Schema.define(version: 2019_09_10_084915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["full_simulation_id"], name: "index_gas_simulations_on_full_simulation_id"
-
   end
 
   create_table "users", force: :cascade do |t|
