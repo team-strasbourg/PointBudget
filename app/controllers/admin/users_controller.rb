c5b54e56c7f9eeb0df45921ff7a6252c3c37cdd7
Carsano <damien.fabiano@gadz.org>
2019-09-10 16:25:01 +0200

    update admin/user_controller for push for jean

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,25 +1,59 @@
 module Admin
   class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
+    def index
+      @users = User.all.sort_by { |user| user.admin ? 0 : 1 }
+    end
 
-  def show
-  end
+    def show
+      @user = User.find(params[:id])
+    end
 
-  def new
-  end
+    def new
+      @user = User.new
+    end
 
-  def create
-  end
+    def create
+      @user = User.new(email: params[:user][:email],
+                       password: params[:user][:password])
+      if @user.save
+        Cart.create(user: @user)
+        flash[:success]='User created'
+        redirect_to admin_root_path
+      else
+        redirect_to new_admin_user_path
+      end
+    end
 
-  def edit
-  end
+    def edit
+      @user = User.friendly.find(params[:id])
+    end
 
-  def update
-  end
+    def update
+      @user = User.friendly.find(params[:id])
+      if @user.update(user_params)
+        redirect_to admin_root_path
+      else
+        render 'edit'
+      end
+    end
 
-  def destroy
+    def destroy
+      @user = User.find(params[:id])
+
+      if @user.admin != true
+        @user.destroy
+        flash[:success] = 'User successfully deleted'
+      else
+        flash[:error] = "You can't destroy a admin user"
+      end
+      redirect_to admin_users_path
+    end
+
+
+    private
+
+    def user_params
+      params[:user].permit(:first_name, :last_name, :description, :admin)
+    end
   end
-end
 end
