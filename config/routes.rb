23c698fcd66fe0e3787dc53d91db85c789314911
Nadrojz <jor.perrin@gmail.com>
2019-09-10 16:08:48 +0200

    Added routes and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,15 @@
 Rails.application.routes.draw do
 
-  get 'full_simulations/new'
-  get 'full_simulations/create'
-  get 'full_simulations/update'
-  get 'full_simulations/edit'
-  get 'full_simulations/destroy'
-  get 'full_simulations/index'
-  get 'full_simulations/show'
   devise_for :users
 
   root :to => "static_pages#landing_page"
   get 'static_pages/about'
   get 'static_pages/contact'
-  resources :energies, only: [:show]
+  resources :energies, only: [:show, :new]
 
   resources :gas_simulations
   resources :users
-
+  resources :full_simulations
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
