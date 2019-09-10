6886736f21c0316484d1da746673fa2b8031764c
Nadrojz <jor.perrin@gmail.com>
2019-09-10 14:31:11 +0200

    Created routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   resources :energies, only: [:show]
+  resources :gassimulations
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
