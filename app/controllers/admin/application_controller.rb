05f3a0e9f3efbfbad6103f5ffaab74868a999655
Carsano <damien.fabiano@gadz.org>
2019-09-10 15:57:33 +0200

    add permission helper is admin

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,15 @@
 module Admin
   class ApplicationController < ::ActionController
+
+    before_action :only_admin
+
+    private
+
+    def only_admin
+      unless user_signed_in? && current_user.is_admin == true
+        flash[:danger] = 'You\'re not allowed on this page'
+        redirect_to items_path
+      end
+    end
   end
 end
