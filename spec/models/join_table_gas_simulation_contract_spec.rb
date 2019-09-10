8495490c7dd7c49a4601fe6e485dffea429d74a5
Carsano <damien.fabiano@gadz.org>
2019-09-10 13:58:21 +0200

    do the validation and associations test

---
+++ spec/models/join_table_gas_simulation_contract_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe JoinTableGasSimulationContract, type: :model do
+  before(:each) do
+    @join_table_gas_simulation_contract = create(:join_table_gas_simulation_contract)
+  end
+
+  context "validations" do
+
+    it "is valid with valid attributes" do
+      expect(@join_table_gas_simulation_contract).to be_a(JoinTableGasSimulationContract)
+      expect(@join_table_gas_simulation_contract).to be_valid
+    end
+
+  end
+  context "associations" do
+    describe "gas_simulation" do
+      it "belongs to one gas_simulation" do
+        expect(@join_table_gas_simulation_contract).to have_one(:gas_simulation)
+      end
+    end
+
+    describe "gas_contract" do
+      it "belongs to one gas_contract" do
+        expect(@join_table_gas_simulation_contract).to have_one(:gas_contract)
+      end
+    end
+  end
+end
