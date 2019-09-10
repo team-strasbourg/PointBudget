a74154ea0f32792b39aeb5492cdcbfd781c02ef4
Nadrojz <jor.perrin@gmail.com>
2019-09-10 09:47:45 +0200

    Spec FullSimulation bad cost created

--- spec/factories/full_simulations.rb
+++ spec/factories/full_simulations.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :full_simulation do
-    total_cost_saved { 1.5 }
+    total_cost_saved { 1.56 }
     validated { false }
+
+    factory :full_simulation_bad_cost do
+      total_cost_saved {1.5}
+    end
+
+
   end
 end
