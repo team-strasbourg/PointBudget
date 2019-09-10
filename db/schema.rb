ec89702deca728435b8d0542e4a2046c58fb0efd
Nadrojz <jor.perrin@gmail.com>
2019-09-10 10:04:38 +0200

    Devise migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_10_073513) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "full_simulations", force: :cascade do |t|
     t.float "total_cost_saved"
@@ -25,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_073513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_full_simulations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -42,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_09_10_073513) do
     t.index ["city_id"], name: "index_users_on_city_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
   end
 
 end
