116a246f6f2e077bab7bc83e7548b27eb6241b31
iroshiva <jean.weil@protonmail.com>
2019-09-10 22:21:05 +0200

    set controller users show edit update + render partial form in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    unless current_user.id == @user.id
+      flash[:error] = "You can't go to other user's profile!!"
+      redirect_to user_path(current_user)
+    end
   end
 
   def new
@@ -12,11 +17,25 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      flash.now[:success] = "Votre profil vient d'être édité avec succès!"
+      redirect_to user_path(@user.id)
+    else
+      flash.now[:error] = "Votre profil n'a pas pu être édité..."
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  def user_params
+    params[:user].permit(:first_name, :last_name, :phone_number)
   end
 end
