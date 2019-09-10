c12e40f06a5b158a5df6a966150206e3c0ff2e08
iroshiva <jean.weil@protonmail.com>
2019-09-10 10:30:46 +0200

    add files vendor of kit_ui in vendor app + add require js in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,11 @@
 //= require_tree .
 
 //= require js/pixel
+
+//= require vendor/jquery/jquery.min
+//= require vendor/popper/popper.min
+//= require vendor/bootstrap/bootstrap.min
+//= require vendor/headroom/headroom.min
+//= require vendor/bootstrap-tagsinput/bootstrap-tagsinput.min
+//= require vendor/waypoints/jquery.waypoints.min
+//= require vendor/smooth-scroll/smooth-scroll.polyfills.min
