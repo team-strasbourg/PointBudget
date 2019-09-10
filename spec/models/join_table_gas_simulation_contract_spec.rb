2b3b7c4c344921d6692684eaa06c5d150b059559
Carsano <damien.fabiano@gadz.org>
2019-09-10 14:18:38 +0200

    update thanks to rubocop

--- spec/models/join_table_gas_simulation_contract_spec.rb
+++ spec/models/join_table_gas_simulation_contract_spec.rb
@@ -5,23 +5,23 @@ RSpec.describe JoinTableGasSimulationContract, type: :model do
     @join_table_gas_simulation_contract = create(:join_table_gas_simulation_contract)
   end
 
-  context "validations" do
+  context 'validations' do
 
-    it "is valid with valid attributes" do
+    it 'is valid with valid attributes' do
       expect(@join_table_gas_simulation_contract).to be_a(JoinTableGasSimulationContract)
       expect(@join_table_gas_simulation_contract).to be_valid
     end
 
   end
-  context "associations" do
-    describe "gas_simulation" do
-      it "belongs to one gas_simulation" do
+  context 'associations' do
+    describe 'gas_simulation' do
+      it 'belongs to one gas_simulation' do
         expect(@join_table_gas_simulation_contract).to belong_to(:gas_simulation)
       end
     end
 
-    describe "gas_contract" do
-      it "belongs to one gas_contract" do
+    describe 'gas_contract' do
+      it 'belongs to one gas_contract' do
         expect(@join_table_gas_simulation_contract).to belong_to(:gas_contract)
       end
     end
