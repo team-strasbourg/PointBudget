30496f78b246a21f9f80a46069ec939da2da9eda
Carsano <damien.fabiano@gadz.org>
2019-09-10 15:50:34 +0200

    update routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root :to => "static_pages#landing_page"
+  root :to => 'static_pages#landing_page'
   get 'static_pages/about'
   get 'static_pages/contact'
   resources :energies, only: [:show]
@@ -10,6 +10,13 @@ Rails.application.routes.draw do
   resources :gas_simulations
   resources :users
 
+  namespace :admin do
+    root :to =>'users#index'
+    resources :users
+    resources :energies, only: [:show]
+    resources :gas_simulations
+    resources :full_simulations
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
