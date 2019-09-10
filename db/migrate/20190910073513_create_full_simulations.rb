78da15ab2f9445e926cd6826ca452de8475e0a07
Nadrojz <jor.perrin@gmail.com>
2019-09-10 09:39:07 +0200

    Add migration for FullSimulation

--- db/migrate/20190910073513_create_full_simulations.rb
+++ db/migrate/20190910073513_create_full_simulations.rb
@@ -3,7 +3,7 @@ class CreateFullSimulations < ActiveRecord::Migration[5.2]
     create_table :full_simulations do |t|
       t.float :total_cost_saved
       t.boolean :validated
-
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
