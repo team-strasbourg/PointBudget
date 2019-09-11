361512d299d6a580906de90115e07c51520e9d17
iroshiva <jean.weil@protonmail.com>
2019-09-11 13:15:31 +0200

    redirection after update password and email resgiration#edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
-		super
 		if current_user.is_admin?
 			stored_location_for(resource) || admin_root_path
 		else
       stored_location_for(resource) || user_path(current_user)
     end
   end
+
+  # def after_update_path_for(resource)
+  #   stored_location_for(resource) || user_path(current_user)
+  # end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,13 +38,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
