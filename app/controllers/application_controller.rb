05840c51f124b7d8da42b2a743ed914cff577dd4
iroshiva <jean.weil@protonmail.com>
2019-09-11 10:42:03 +0200

    change path for change password in users#show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
+		super
 		if current_user.is_admin?
 			stored_location_for(resource) || admin_root_path
 		else
