6eaaf48f14f04a71b7fe90b27d7b8889cf29b574
iroshiva <jean.weil@protonmail.com>
2019-09-11 13:34:37 +0200

    rerouting in dashborad user#show failed...

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,22 +4,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  # # GET /resource/sign_up
+  # # def new
+  # #   super
+  # # end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  # # POST /resource
+  # # def create
+  # #   super
+  # # end
 
-  # GET /resource/edit
+  # # GET /resource/edit
   # def edit
   #   super
   # end
 
-  # PUT /resource
+   # PUT /resource
   # def update
   #   super
   # end
@@ -38,27 +38,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  protected
+  # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
+  # # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
-  # If you have extra params to permit, append them to the sanitizer.
+  # # def after_update_path_for(resource)
+  # #   user_root_path(resource)
+  # # end
+  # # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
   #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
   # end
 
-  # The path used after sign up.
+  # # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
   # end
 
-  # The path used after sign up for inactive accounts.
+  # # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
