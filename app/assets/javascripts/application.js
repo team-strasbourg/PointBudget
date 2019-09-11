7aca15b46a1692cd9f4171b897d6b431dfc7367e
Nadrojz <jor.perrin@gmail.com>
2019-09-11 18:11:32 +0200

    Finished javascript for radio buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,16 +32,26 @@
 
 $(document).ready(function () {
 
+
+  let knowConsumptionStatus = true
+
   radioYes = document.getElementById("answer_yes")
   radioNo = document.getElementById("answer_no")
 
   radioYes.addEventListener('click', function(){
-    document.getElementById('consumption').style.display = "block"
+
+      document.getElementById('consumption').style.display = "block"
+      document.getElementById('estimation').style.display = "none"
+
   });
 
 
   radioNo.addEventListener('click', function(){
-    document.getElementById('estimation').style.display = "block"
+
+      document.getElementById('estimation').style.display = "block"
+      document.getElementById('consumption').style.display = "none"
+
   });
+
 
 });
