33db36b0e7cffb30c9f85ecde074bd50673cce1e
Carsano <damien.fabiano@gadz.org>
2019-09-11 13:09:32 +0200

    can edit gaz contract

--- app/controllers/admin/gas_contracts_controller.rb
+++ app/controllers/admin/gas_contracts_controller.rb
@@ -11,13 +11,12 @@ module Admin
 
     def create
       my_params=params[:gas_contract]
-      @contract = GasContract.new(
-          supplier: my_params[:supplier],
-          offer_name: my_params[:offer_name],
-          subscription_base_price_month: my_params[:subscription_base_price_month],
-          kwh_price_base: my_params[:kwh_price_base],
-          low_kw_consumption_per_year: my_params[:low_kw_consumption_per_year],
-          high_kw_consumption_per_year: my_params[:high_kw_consumption_per_year]
+      @contract = GasContract.new(supplier: my_params[:supplier],
+                                  offer_name: my_params[:offer_name],
+                                  subscription_base_price_month: my_params[:subscription_base_price_month],
+                                  kwh_price_base: my_params[:kwh_price_base],
+                                  low_kw_consumption_per_year: my_params[:low_kw_consumption_per_year],
+                                  high_kw_consumption_per_year: my_params[:high_kw_consumption_per_year]
       )
       if @contract.save
         flash[:success]='Contract created'
@@ -27,5 +26,27 @@ module Admin
       end
     end
 
+    def edit
+      @contract = GasContract.find(params[:id])
+    end
+
+    def update
+      @contract = GasContract.find(params[:id])
+      if @contract.update(contract_params)
+        redirect_to admin_root_path
+      else
+        render 'edit'
+      end
+    end
+    private
+
+    def contract_params
+      params.require(:gas_contract).permit(:supplier,
+                                           :offer_name,
+                                           :subscription_base_price_month,
+                                           :kwh_price_base,
+                                           :low_kw_consumption_per_year,
+                                           :high_kw_consumption_per_year)
+    end
   end
 end
