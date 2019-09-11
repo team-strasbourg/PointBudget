694165bd13afa908ed8d1ae6a3b09c8a6d53f330
Carsano <damien.fabiano@gadz.org>
2019-09-11 21:45:05 +0200

    pb fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {
-      registration: 'users/registrations'
-  }
+  devise_for :users, :controllers => { :registrations => :registrations }
 
   root :to => 'static_pages#landing_page'
   get 'static_pages/about'
   get 'static_pages/contact'
-
 
 
   namespace :admin do
@@ -20,15 +17,14 @@ Rails.application.routes.draw do
   end
 
   resources :users do
+    root :to => "users#show"
     resources :full_simulations do
       resources :energies, only: [:show, :new]
       resources :gas_simulations
     end
   end
 
-  namespace :users do
-    root :to => "users#show"
-  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
