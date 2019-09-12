9380382dfe42a7ea2c14c3edd726a9bcdec78aca
Carsano <damien.fabiano@gadz.org>
2019-09-12 11:43:18 +0200

    uncomment js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,22 +33,22 @@
 $(document).ready(function () {
 
 
-  // radioYes = document.getElementById("answer_yes")
-  // radioNo = document.getElementById("answer_no")
-  //
-  // radioYes.addEventListener('click', function(){
-  //
-  //     document.getElementById('consumption').style.display = "block"
-  //     document.getElementById('estimation').style.display = "none"
-  //
-  // });
-  //
-  //
-  // radioNo.addEventListener('click', function(){
-  //
-  //     document.getElementById('estimation').style.display = "block"
-  //     document.getElementById('consumption').style.display = "none"
-  //
-  // });
+  radioYes = document.getElementById("answer_yes")
+  radioNo = document.getElementById("answer_no")
+
+  radioYes.addEventListener('click', function(){
+
+      document.getElementById('consumption').style.display = "block"
+      document.getElementById('estimation').style.display = "none"
+
+  });
+
+
+  radioNo.addEventListener('click', function(){
+
+      document.getElementById('estimation').style.display = "block"
+      document.getElementById('consumption').style.display = "none"
+
+  });
 
 });
