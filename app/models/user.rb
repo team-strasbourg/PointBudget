36399030e1bfb54e42c0c10dcc18adda2e19449a
Carsano <damien.fabiano@gadz.org>
2019-09-12 13:37:11 +0200

    add method welcome send in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  after_create :welcome_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -14,8 +15,9 @@ class User < ApplicationRecord
   def has_city
     city_id.nil? || city_id.zero? ? false : true
   end
+  private
 
-  def zip_code
-    city.zip_code
+  def welcome_send
+    UserMailer.welcome_email(self).deliver.now
   end
 end
