033c9ea74f6fb3d72dab96c38ab500580f36fabf
Carsano <damien.fabiano@gadz.org>
2019-09-12 15:43:47 +0200

    refactor def in fulll_simulation.rb

--- app/models/full_simulation.rb
+++ app/models/full_simulation.rb
@@ -4,18 +4,12 @@ class FullSimulation < ApplicationRecord
   after_update :mail_if_validated
 
   def only_one_gas_simulation
-    if gas_simulation == nil
-      false
-    else
-    	true
-    end
+    gas_simulation.nil? ? false : true
   end
 
   private
 
   def mail_if_validated
-    if validated
-      UserMailer.simulation_validated_email(self.user).deliver_now
-    end
+    UserMailer.simulation_validated_email(self.user).deliver_now if validated
   end
 end
