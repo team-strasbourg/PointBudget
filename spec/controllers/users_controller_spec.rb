ce93a1a1381623923d68c846d82b0046ec490a1f
Carsano <damien.fabiano@gadz.org>
2019-09-12 19:11:54 +0200

    2 test for show controllers works

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET show' do
-    login_user
+
 
     it "should have a current_user" do
-      # note the fact that you should remove the "validate_session" parameter if this was a scaffold-generated controller
+      login_user
       expect(subject.current_user).to_not eq(nil)
     end
 
 
     it 'renders the show template' do
+      login_user
       get :show
       expect(response).to render_template('show')
     end
