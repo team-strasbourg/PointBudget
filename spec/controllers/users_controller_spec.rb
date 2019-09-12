ba35c0d11708051e39f6d919f7e898cc5af3096b
Carsano <damien.fabiano@gadz.org>
2019-09-12 19:02:07 +0200

    add require relative controller_macros in rails_helper

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET show' do
+    login_user
     it 'assigns @users' do
-      user = build(:user)
+
+
       get :show, id: user.id
       expect(assigns(:user)).to eq(user)
     end
