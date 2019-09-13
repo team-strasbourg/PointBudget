c7920730df6e192daccc625641af41924028d35e
Carsano <damien.fabiano@gadz.org>
2019-09-13 11:06:36 +0200

    first tests hash for consumption people

--- spec/models/gas_simulation_spec.rb
+++ spec/models/gas_simulation_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe GasSimulation, type: :model do
     @gas_simulation = create(:gas_simulation)
     end
 
-  context "validations" do
+  context 'validations' do
 
-    it "is valid with valid attributes" do
+    it 'is valid with valid attributes' do
         expect(@gas_simulation).to be_a(GasSimulation)
         expect(@gas_simulation).to be_valid
     end
 
-    describe "#actual_price_paid" do
+    describe '#actual_price_paid' do
       it 'should have an actual paid price' do
         expect(build(:gas_empty_price_paid)).not_to be_valid
       end
@@ -22,7 +22,7 @@ RSpec.describe GasSimulation, type: :model do
       end
     end
 
-    describe "#gas_cost_saved" do
+    describe '#gas_cost_saved' do
       it 'should have an gas_cost_saved' do
         expect(build(:gas_empty_cost_saved)).not_to be_valid
       end
@@ -67,7 +67,7 @@ RSpec.describe GasSimulation, type: :model do
       end
     end
 
-    describe "#residents_number" do
+    describe '#residents_number' do
       it 'can be blank' do
         expect(build(:gas_simulation, residents_number: nil)).to be_valid
       end
@@ -93,18 +93,17 @@ RSpec.describe GasSimulation, type: :model do
     end
   end
 
-  context "associations" do
+  context 'associations' do
 
-    describe "gas_simulation" do
-      it "belongs to one full_simulation" do
+    describe 'gas_simulation' do
+      it 'belongs to one full_simulation' do
         expect(@gas_simulation).to belong_to(:full_simulation)
       end
     end
 
   end
 
-  context "public methods" do
-
+  context 'public methods' do
     describe '#consumption_people' do
       it 'should return 1630 if 1' do
         expect(@gas_simulation.consumption_people(1)).to eq(1630)
@@ -126,6 +125,9 @@ RSpec.describe GasSimulation, type: :model do
         expect(@gas_simulation.consumption_people(5)).to eq(6360)
       end
 
+      it 'should return 7360 if 6' do
+        expect(@gas_simulation.consumption_people(6)).to eq(7360)
+      end
     end
 
   end
