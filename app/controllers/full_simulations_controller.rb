32ea069248e8537bc776acaa0a23ccb096c5f899
iroshiva <jean.weil@protonmail.com>
2019-09-13 14:09:38 +0200

    add destroy method in controller full_simulations

--- app/controllers/full_simulations_controller.rb
+++ app/controllers/full_simulations_controller.rb
@@ -39,6 +39,15 @@ class FullSimulationsController < ApplicationController
   end
 
   def destroy
+    @full_simulation = FullSimulation.find(params[:id])
+
+    if @full_simulation.destroy
+      flash[:success] = "La simulation a bien été supprimée"
+      redirect_to user_full_simulations_path(current_user)
+    else
+      flash[:error] = "La simulation n'a pa pu être supprimée"
+      redirect_to user_full_simulations_path(current_user)
+    end
   end
 
   private
