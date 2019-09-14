2b17c73be783f13e8a8acb4c88f69285ab88e946
Carsano <damien.fabiano@gadz.org>
2019-09-14 20:51:35 +0200

    test for edit user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,6 +43,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   # For Devise > 4.1.1
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
   # Use the following instead if you are on Devise <= 4.1.1
   # config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
