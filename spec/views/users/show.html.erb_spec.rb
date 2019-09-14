580e1bb453f7fd1ff0692b764990ad3e606ae4c3
Carsano <damien.fabiano@gadz.org>
2019-09-14 20:53:36 +0200

    finished SHOW test user

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", type: :view do
-  describe 'it says Mon Profile' do
-    before :each do
-      @user = create(:user)
-    end
-    it "displays 'Mon Profile'" do
-      sign_in @user
-      render
-      expect(rendered).to have_content 'Mon Profil'
-    end
 
-    it "displays the mail" do
-      sign_in @user
-      render
-      expect(rendered).to have_content @user.email
-    end
+  before :each do
+    @user = create(:user)
+  end
+  it "displays 'Mon Profile'" do
+    sign_in @user
+    render
+    expect(rendered).to have_content 'Mon Profil'
+  end
 
-    it "displays have link to edition" do
-      sign_in @user
-      render
-      expect(rendered).to have_content 'Editer mon profil'
-      expect(rendered).to have_content 'Editer mon compte'
-    end
+  it "displays the mail" do
+    sign_in @user
+    render
+    expect(rendered).to have_content @user.email
+  end
+
+  it "displays have link to edition" do
+    sign_in @user
+    render
+    expect(rendered).to have_content 'Editer mon profil'
+    expect(rendered).to have_content 'Editer mon compte'
   end
 end
