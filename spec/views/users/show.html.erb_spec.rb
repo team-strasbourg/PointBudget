af1cc5eadb83ff6d48b3d80f09fd2b28191573fd
Carsano <damien.fabiano@gadz.org>
2019-09-14 19:56:58 +0200

    add first test for view edit user

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "users/show.html.erb", type: :view do
-
+  context 'it says Mon Profile' do
+    it "displays 'Mon Profile'" do
+      render
+      expect(rendered).to have_content 'My Profile'
+    end
+  end
 end
