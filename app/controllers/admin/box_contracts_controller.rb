ef31d1b1e670e0e036810da60c533e97e70abc3a
Carsano <damien.fabiano@gadz.org>
2019-09-16 21:31:19 +0200

    create controller admin for box contracts

---
+++ app/controllers/admin/box_contracts_controller.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module Admin
+  class BoxContractsController < ApplicationController
+    def index
+      @contracts = BooxContract.all.sort_by(&:supplier)
+    end
+
+    def show
+      @contract = BoxContract.find(params[:id])
+    end
+
+    def new
+      @contract = BoxContract.new
+    end
+
+    def create
+      my_params = params[:box_contract]
+      @contract = BoxContract.new(supplier: my_params[:supplier],
+                                  offer_name: my_params[:offer_name],
+                                  price_month: my_params[:price_month],
+                                  commitment: my_params[:commitment],
+                                  price_after: my_params[:price_after],
+                                  type: my_params[:type],
+                                  downstream: my_params[:downstream],
+                                  upstream: my_params[:upstream],
+                                  tv_channel: my_params[:tv_channel],
+                                  tv: my_params[:tv],
+                                  call_fix_fr: my_params[:call_fix_fr],
+                                  call_mobile_fr: my_params[:call_mobile_fr],
+                                  call_foreign: my_params[:call_foreign],
+                                  opening_fee: my_params[:opening_fee],
+                                  termination_fee: my_params[:termination_fee],
+                                  taken_termination: my_params[:taken_termination])
+      if @contract.save
+        flash[:success] = 'Contract created'
+        redirect_to admin_box_contracts_path
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @contract = BoxContract.find(params[:id])
+    end
+
+    def update
+      @contract = BoxContract.find(params[:id])
+      if @contract.update(contract_params)
+        redirect_to admin_gas_contract_path
+      else
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @contract = BoxContract.find(params[:id])
+      @contract.destroy
+      flash[:success] = 'User successfully deleted'
+      redirect_to admin_gas_contracts_path
+    end
+
+    private
+
+    def contract_params
+      params.require(:gas_contract).permit(:supplier,
+                                           :offer_name,
+                                           :price_month,
+                                           :commitment,
+                                           :price_after,
+                                           :type,
+                                           :downstream,
+                                           :upstream,
+                                           :tv_channel,
+                                           :tv,
+                                           :call_fix_fr,
+                                           :call_mobile_fr,
+                                           :call_foreign,
+                                           :opening_fee,
+                                           :termination_fee,
+                                           :taken_termination)
+    end
+  end
+end
