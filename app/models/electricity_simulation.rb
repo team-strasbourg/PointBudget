43e0c122eb87dfa1cc63ee6a9b243c935717294a
Nadrojz <jor.perrin@gmail.com>
2019-09-16 19:26:22 +0200

    modified dependencies

--- app/models/electricity_simulation.rb
+++ app/models/electricity_simulation.rb
@@ -1,5 +1,5 @@
 class ElectricitySimulation < ApplicationRecord
   belongs_to :full_simulation
   has_many :join_table_elec_simulation_contracts, dependent: :destroy
-  has_many :elec_contracts, through: :join_table_elec_simulation_contracts
+  has_many :electricity_contracts, through: :join_table_electricity_simulation_contracts
 end
