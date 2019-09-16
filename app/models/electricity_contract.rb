d9d8c07074a7437143441dbefdc2ffa1966a8841
Nadrojz <jor.perrin@gmail.com>
2019-09-16 18:58:07 +0200

    added dependencies

--- app/models/electricity_contract.rb
+++ app/models/electricity_contract.rb
@@ -1,2 +1,10 @@
 class ElectricityContract < ApplicationRecord
+  has_many :join_table_electricity_simulation_contracts, dependent: :destroy
+  has_many :electricity_simulations, through: :join_table_electricity_simulation_contracts
+
+  validates :subscription_base_price_month,
+            numericality: { less_than_or_equal_to: 40.00,
+                            greater_than_or_equal_to: 6.00 }
+  validates :kwh_price_base,
+            numericality: { less_than_or_equal_to: 0.10000, greater_than_or_equal_to: 0.04000 }
 end
