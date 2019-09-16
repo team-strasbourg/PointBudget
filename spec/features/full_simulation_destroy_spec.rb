dbb3f7773817be496fc40e0e1cd1fd1128f6577e
Carsano <damien.fabiano@gadz.org>
2019-09-16 13:44:51 +0200

    add destroy test for non validated simulation

--- spec/features/full_simulation_destroy_spec.rb
+++ spec/features/full_simulation_destroy_spec.rb
@@ -2,5 +2,32 @@
 
 require 'rails_helper'
 RSpec.describe 'Full Simulation', type: :feature do
+  before :each do
+    @user = User.create(email: 'user@example.com', password: 'password')
+    sign_in @user.email, @user.password
+    5.times do
+      create(:full_simulation, user: @user, validated: true)
+      create(:full_simulation, user: @user)
+    end
+  end
 
+  scenario 'delete a non validated simulation' do
+    visit user_full_simulations_path(@user)
+    full_sim_id = FullSimulation.last.id
+    within("#simulation-unconfirmed-#{full_sim_id}") do
+      click_link 'Supprimer'
+    end
+    expect(page).not_to have_content(full_sim_id)
+    expect(page).to have_content(full_sim_id - 1)
+  end
+
+  scenario 'delete a validated simulation' do
+    visit user_full_simulations_path(@user)
+    full_sim_id = FullSimulation.last.id-1
+    within("#simulation-confirmed-#{full_sim_id}") do
+      click_link 'Supprimer'
+    end
+    expect(page).not_to have_content(full_sim_id)
+    expect(page).to have_content(full_sim_id - 1)
+  end
 end
