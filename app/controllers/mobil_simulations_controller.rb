039b1f7f1233d1b855886e42235947e9d680b6f3
iroshiva <jean.weil@protonmail.com>
2019-09-17 18:29:24 +0200

    start controller mobil methods

--- app/controllers/mobil_simulations_controller.rb
+++ app/controllers/mobil_simulations_controller.rb
@@ -1,14 +1,19 @@
 class MobilSimulationsController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!
+  before_action :user_signed_in?
+
+  def index; end
 
   def show
+    @mobil_simu = MobilSimulation.find(params[:id])
   end
 
   def new
+    @mobil_simu = MobilSimulation.new
   end
 
   def create
+    @mobil_simu = MobilSimulation.find(params[:id])
   end
 
   def edit
