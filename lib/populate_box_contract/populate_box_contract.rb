9180530522e8f7111db6b9bad43641daf6b1d889
Giancarlo <gpbene@gmail.com>
2019-09-17 09:17:15 +0200

    replace Gas with Box

--- lib/populate_box_contract/populate_box_contract.rb
+++ lib/populate_box_contract/populate_box_contract.rb
@@ -1,12 +1,12 @@
 # BOX contract
 require 'csv'
 
-data = CSV.read('./lib/populate_gas_contract/offer_gas.csv',
-                headers: false, col_sep: ',', encoding: 'ISO-8859-1')
+data = CSV.read('./lib/populate_box_contract/offer_box.csv',
+                headers: true, col_sep: ',', encoding: 'ISO-8859-1')
 lines = data.reject { |line| line[0].nil? }
-GasContract.destroy_all
+BoxContract.destroy_all
 lines.each do |line|
-  GasContract.create(supplier: line[0],
+  BoxContract.create(supplier: line[0],
                      offer_name: line[1],
                      low_kw_consumption_per_year: line[2],
                      high_kw_consumption_per_year: line[3],
