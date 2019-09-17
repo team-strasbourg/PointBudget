12f754af32e95a9218202fd42169c44576c32ea6
Carsano <damien.fabiano@gadz.org>
2019-09-17 22:25:24 +0200

    comment the gas simulation

--- app/models/gas_simulation.rb
+++ app/models/gas_simulation.rb
@@ -40,6 +40,7 @@ class GasSimulation < ApplicationRecord
     self.full_simulation.user
   end
 
+  # This method can estimate the consumption depending on the params you give to it
   def estimation(yearly_cost, yearly_consumption, floor_space, heat_type, water_cooking_type, nb_residents )
     yearly_cost = yearly_cost.to_i
     yearly_consumption = yearly_consumption.to_i
@@ -94,6 +95,7 @@ class GasSimulation < ApplicationRecord
     return_array
   end
 
+  # Estimate the consumption per habitant
   def consumption_people(nb_residents)
     hash = { 1 => 1630, 2 => 2945, 3 => 4265, 4 => 5320, 5 => 6360 }
     if hash[nb_residents].nil?
@@ -103,11 +105,13 @@ class GasSimulation < ApplicationRecord
     end
   end
 
+  # This method is part of the estimation process
+  # It verifies the entries of the client
   def verify_nilness_params(yearly_cost, yearly_consumption, floor_space, heat_type, water_cooking_type, nb_residents)
-    if yearly_cost.zero?
+    if yearly_cost.zero? # if he forgot the yearly cost
       false
     else
-      if yearly_consumption.zero?
+      if yearly_consumption.zero? # if the consumption is not entered, all the other field must be present
         if [floor_space, nb_residents].include?(0) || [heat_type, water_cooking_type].include?('')
           false
         else
@@ -118,6 +122,4 @@ class GasSimulation < ApplicationRecord
       end
     end
   end
-
-
 end
