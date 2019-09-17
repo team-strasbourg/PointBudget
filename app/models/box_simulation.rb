6b664bfcfe7a80f0da76887aeeecc3083e51da86
Carsano <damien.fabiano@gadz.org>
2019-09-17 10:30:28 +0200

    add validation in model box

--- app/models/box_simulation.rb
+++ app/models/box_simulation.rb
@@ -3,6 +3,19 @@ class BoxSimulation < ApplicationRecord
   has_many :join_table_box_contracts, dependent: :destroy
   has_many :box_contracts, through: :join_table_box_contracts
 
+  validates :actual_price_paid,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+  validates :box_cost_saved,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+  validates :tv,
+            presence: true
+  validates :call_fix_fr,
+            presence: true
+  validates :call_mob_fr,
+            presence: true
+
   def print_report
     table_attributes = []
     [tv, call_fix_fr, call_mob_fr].each do |attribute|
