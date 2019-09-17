f638a62da9eba4807aefa4f5bfed28f141cf19b4
Nadrojz <jor.perrin@gmail.com>
2019-09-17 10:27:50 +0200

    added ele routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
     root to: 'users#index'
     resources :users
     resources :gas_contracts
+    resources :ele_contracts
   end
 
   resources :users, only: %i[show edit update] do
@@ -18,6 +19,7 @@ Rails.application.routes.draw do
     resources :full_simulations do
       resources :energies, only: %i[show new]
       resources :gas_simulations, except: [:index]
+      resources :ele_simulations, except: [:index]
     end
   end
 end
