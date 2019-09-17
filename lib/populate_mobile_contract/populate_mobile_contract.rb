d6ff9e7b365140e4fc74a5bba95c333a36e668f8
Giancarlo <gpbene@gmail.com>
2019-09-17 12:21:38 +0200

    delete empty columns from csv file

--- lib/populate_mobile_contract/populate_mobile_contract.rb
+++ lib/populate_mobile_contract/populate_mobile_contract.rb
@@ -1,15 +1,16 @@
 require 'csv'
 
-data = CSV.read('./lib/populate_elec_contract/offer_electricity.csv',
+data = CSV.read('./lib/populate_mobile_contract/offer_mobile.csv',
 headers: true, col_sep: ',', encoding: 'ISO-8859-1')
 lines = data.reject { |line| line[0].blank? }
+puts lines
 
-EleContract.destroy_all
-lines.each do |line|
-  EleContract.create(supplier: line[0],
-      offer_name: line[1],
-      kVA_power: line[2].to_i,
-      subscription_base_price_month: line[3].to_f,
-      kwh_price_base: line[4].to_f
-      )
-end
+# EleContract.destroy_all
+# lines.each do |line|
+#   EleContract.create(supplier: line[0],
+#       offer_name: line[1],
+#       kVA_power: line[2].to_i,
+#       subscription_base_price_month: line[3].to_f,
+#       kwh_price_base: line[4].to_f
+#       )
+# end
