5cd2f54e40ab0b545a2d464eaab67a806769ffa6
iroshiva <jean.weil@protonmail.com>
2019-09-17 10:16:55 +0200

    set attribute to model jointablemobil + migration attached

--- app/models/join_table_mobil_contract.rb
+++ app/models/join_table_mobil_contract.rb
@@ -1,2 +1,4 @@
 class JoinTableMobilContract < ApplicationRecord
+	belongs_to :join_table_mobil_contract
+  belongs_to :mobil_contract
 end
--- db/migrate/20190917081407_create_join_table_mobil_contracts.rb
+++ db/migrate/20190917081407_create_join_table_mobil_contracts.rb
@@ -2,7 +2,8 @@ class CreateJoinTableMobilContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :join_table_mobil_contracts do |t|
       t.float :savings
-
+      t.belongs_to :mobil_simulation, index: true
+			t.belongs_to :mobil_contract, index: true
       t.timestamps
     end
   end
