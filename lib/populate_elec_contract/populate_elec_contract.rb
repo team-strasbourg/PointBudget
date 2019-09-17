86289c485cba7201f363e8e1dc75ee81e0590b7f
Giancarlo <gpbene@gmail.com>
2019-09-17 10:46:09 +0200

    link to csv file

---
+++ lib/populate_elec_contract/populate_elec_contract.rb
@@ -0,0 +1,28 @@
+require 'csv'
+
+data = CSV.read('./lib/populate_elec_contract/offer_electricity.csv',
+headers: true, col_sep: ',', encoding: 'ISO-8859-1')
+lines = data.reject { |line| line[0].blank? }
+
+puts lines[3][2]
+
+# EleContract.destroy_all
+# lines.each do |line|
+# EleContract.create(supplier: line[0],
+#      offer_name: line[1],
+#      price_month: line[2].to_i,
+#      engagement: line[3].to_i,
+#      price_after: line[4].to_i,
+#      type: line[5],
+#      downstream: line[6].to_i,
+#      upstream: line[7].to_i,
+#      tv_channel: line[8],
+#      tv: to_boolean(line[9]),
+#      call_fix_fr: to_boolean(line[10]),
+#      call_mobile_fr: to_boolean(line[11]),
+#      call_foreign: to_boolean(line[12]),
+#      opening_fee: line[13].to_i,
+#      termination_fee: line[14].to_i,
+#      taken_termination: line[15].to_i
+#      )
+# end
