8c1e68411be44c5944bda054f043025bb53fae64
Giancarlo <gpbene@gmail.com>
2019-09-17 11:03:29 +0200

    tested box_contracts, add attributes to elec_contracts

--- lib/populate_elec_contract/populate_elec_contract.rb
+++ lib/populate_elec_contract/populate_elec_contract.rb
@@ -6,23 +6,23 @@ lines = data.reject { |line| line[0].blank? }
 
 puts lines[3][2]
 
-# EleContract.destroy_all
-# lines.each do |line|
-# EleContract.create(supplier: line[0],
-#      offer_name: line[1],
-#      price_month: line[2].to_i,
-#      engagement: line[3].to_i,
-#      price_after: line[4].to_i,
-#      type: line[5],
-#      downstream: line[6].to_i,
-#      upstream: line[7].to_i,
-#      tv_channel: line[8],
-#      tv: to_boolean(line[9]),
-#      call_fix_fr: to_boolean(line[10]),
-#      call_mobile_fr: to_boolean(line[11]),
-#      call_foreign: to_boolean(line[12]),
-#      opening_fee: line[13].to_i,
-#      termination_fee: line[14].to_i,
-#      taken_termination: line[15].to_i
-#      )
+# Verifier les to_f!
+EleContract.destroy_all
+lines.each do |line|
+EleContract.create(supplier: line[0],
+     offer_name: line[1],
+     kVA_power: line[2].to_i,
+     subscription_base_price_month: line[3].to_f,
+     kwh_price_base: line[4].to_f
+     )
+end
+
+# create_table "ele_contracts", force: :cascade do |t|
+#   t.string "supplier"
+#   t.string "offer_name"
+#   t.integer "kVA_power"
+#   t.float "subscription_base_price_month"
+#   t.float "kwh_price_base"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
 # end
