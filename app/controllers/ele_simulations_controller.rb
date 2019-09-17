3fe589f915f052ab16fd1a65e2b5ac210e6a6d36
Nadrojz <jor.perrin@gmail.com>
2019-09-17 12:52:34 +0200

    created ele form

--- app/controllers/ele_simulations_controller.rb
+++ app/controllers/ele_simulations_controller.rb
@@ -29,20 +29,11 @@ class EleSimulationsController < ApplicationController
     @full_simulation = FullSimulation.find(params[:full_simulation_id])
     @ele_simulation = EleSimulation.new
     @ele_simulation.assign_params_from_controller(params)
-    estimation = @ele_simulation.estimation(params[:yearly_cost],
-                                            params[:yearly_consumption],
-                                            params[:floor_space],
-                                            params[:heat_type],
-                                            params[:water_cooking_type],
-                                            params[:nb_residents])
-    comparison = estimation[0] == false ? [-1, false] : @ele_simulation.comparison(estimation[0], estimation[1])
+
+    comparison = @ele_simulation.comparison(params[:yearly_cost],params[:yearly_consumption],params[:kVA_power])
     @ele_simulation = EleSimulation.new(actual_price_paid: params[:yearly_cost],
                                         ele_cost_saved: comparison[0],
-                                        floor_space: params[:floor_space],
-                                        heat_type: params[:heat_type],
-                                        water_cooking_type: params[:water_cooking_type],
-                                        residents_number: params[:nb_residents],
-                                        ele_use: estimation[1],
+                                        ele_use: params[:yearly_consumption],
                                         full_simulation: @full_simulation)
 
     if @ele_simulation.save
--- app/models/ele_simulation.rb
+++ app/models/ele_simulation.rb
@@ -2,4 +2,31 @@ class EleSimulation < ApplicationRecord
   belongs_to :full_simulation
   has_many :join_table_ele_simulation_contracts, dependent: :destroy
   has_many :ele_contracts, through: :join_table_ele_simulation_contracts
+
+  def user
+    self.full_simulation.user
+  end
+
+  def assign_params_from_controller(params)
+    @params = params
+  end
+
+  def comparison(yearly_cost, yearly_consumption, kVA_power)
+    first_filter = EleContract.all.select { |contract|
+      contract.kVA_power == kVA_power
+    }
+    second_filter = first_filter.select{ |contract|
+      yearly_cost > (contract.kwh_price_base * yearly_consumption + contract.subscription_base_price_month * 12)
+    }
+    max_save = 0
+    all_savings = []
+    second_filter.each do |contract|
+      savings = yearly_cost - (contract.kwh_price_base * yearly_consumption + contract.subscription_base_price_month * 12)
+      if savings > max_save
+        max_save = savings
+      end
+      all_savings << savings
+    end
+    [max_save.round(2), second_filter, all_savings]
+  end
 end
