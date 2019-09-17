ad69063fb8fee387537164661fd21b5953cf1bea
Giancarlo <gpbene@gmail.com>
2019-09-17 09:25:23 +0200

    add puts to test

--- lib/populate_box_contract/populate_box_contract.rb
+++ lib/populate_box_contract/populate_box_contract.rb
@@ -4,12 +4,13 @@ require 'csv'
 data = CSV.read('./lib/populate_box_contract/offer_box.csv',
                 headers: true, col_sep: ',', encoding: 'ISO-8859-1')
 lines = data.reject { |line| line[0].nil? }
-BoxContract.destroy_all
-lines.each do |line|
-  BoxContract.create(supplier: line[0],
-                     offer_name: line[1],
-                     low_kw_consumption_per_year: line[2],
-                     high_kw_consumption_per_year: line[3],
-                     subscription_base_price_month: line[4],
-                     kwh_price_base: line[5])
-end
+puts lines
+# BoxContract.destroy_all
+# lines.each do |line|
+#   BoxContract.create(supplier: line[0],
+#                      offer_name: line[1],
+#                      low_kw_consumption_per_year: line[2],
+#                      high_kw_consumption_per_year: line[3],
+#                      subscription_base_price_month: line[4],
+#                      kwh_price_base: line[5])
+# end
