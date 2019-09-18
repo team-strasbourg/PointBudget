78db1302d962c77c5ee50c1ae47e84c63748ab54
Carsano <damien.fabiano@gadz.org>
2019-09-18 19:15:48 +0200

    update bank

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_09_18_142431) do
     t.float "insurance_payment", default: 0.0
     t.float "sms_alert", default: 0.0
     t.float "international_withdraw", default: 0.0
-    t.string "name", default: "Bank"
+    t.string "name", default: "Banque"
     t.bigint "full_simulation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -51,14 +51,14 @@ ActiveRecord::Schema.define(version: 2019_09_18_142431) do
     t.float "price_month", default: 0.0
     t.integer "commitment", default: 0
     t.float "price_after", default: 0.0
-    t.string "type", default: ""
+    t.string "internet_type", default: ""
     t.integer "downstream", default: 0
     t.integer "upstream", default: 0
     t.string "tv_channel", default: ""
-    t.boolean "tv", default: false
-    t.boolean "call_fix_fr", default: false
-    t.boolean "call_mobile_fr", default: false
-    t.boolean "call_foreign", default: false
+    t.boolean "tv", default: true
+    t.boolean "call_fix_fr", default: true
+    t.boolean "call_mobile_fr", default: true
+    t.boolean "call_foreign", default: true
     t.float "opening_fee", default: 0.0
     t.float "termination_fee", default: 0.0
     t.float "taken_termination", default: 0.0
@@ -69,9 +69,10 @@ ActiveRecord::Schema.define(version: 2019_09_18_142431) do
   create_table "box_simulations", force: :cascade do |t|
     t.float "actual_price_paid", default: 0.0
     t.float "box_cost_saved", default: 0.0
-    t.boolean "tv", default: false
-    t.boolean "call_fix_fr", default: false
-    t.boolean "call_mob_fr", default: false
+    t.boolean "tv", default: true
+    t.boolean "call_fix_fr", default: true
+    t.boolean "call_mob_fr", default: true
+    t.string "name", default: "Box Internet"
     t.bigint "full_simulation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
