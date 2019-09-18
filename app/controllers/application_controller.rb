65d896621aec2e7a7565c6a130604a77df55d511
Carsano <damien.fabiano@gadz.org>
2019-09-18 14:09:16 +0200

    redirect to new simulation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     if current_user.is_admin?
       stored_location_for(resource) || admin_root_path
     else
-      stored_location_for(resource) || user_path(current_user)
+      stored_location_for(resource) || new_user_full_simulation_path(current_user)
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
     root to: 'users#index'
     resources :users
     resources :full_simulations, only: [:show]
+    resources :gas_simulations, only: [:show]
+    resources :box_simulations, only: [:show]
+    resources :ele_simulations, only: [:show]
     resources :gas_contracts
     resources :box_contracts
     resources :ele_contracts
