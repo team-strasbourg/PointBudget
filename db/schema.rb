4939acad10d382ddbb8b0e8646d9b2f30dd15fe2
iroshiva <jean.weil@protonmail.com>
2019-09-18 12:09:01 +0200

    insert mobil result in full_simulation#show

--- db/schema.rb
+++ db/schema.rb
@@ -21,14 +21,14 @@ ActiveRecord::Schema.define(version: 2019_09_18_080120) do
     t.float "price_month", default: 0.0
     t.integer "commitment", default: 0
     t.float "price_after", default: 0.0
-    t.string "type", default: ""
+    t.string "internet_type"
     t.integer "downstream", default: 0
     t.integer "upstream", default: 0
     t.string "tv_channel", default: ""
-    t.boolean "tv", default: false
-    t.boolean "call_fix_fr", default: false
-    t.boolean "call_mobile_fr", default: false
-    t.boolean "call_foreign", default: false
+    t.boolean "tv"
+    t.boolean "call_fix_fr"
+    t.boolean "call_mobile_fr"
+    t.boolean "call_foreign"
     t.float "opening_fee", default: 0.0
     t.float "termination_fee", default: 0.0
     t.float "taken_termination", default: 0.0
@@ -39,9 +39,10 @@ ActiveRecord::Schema.define(version: 2019_09_18_080120) do
   create_table "box_simulations", force: :cascade do |t|
     t.float "actual_price_paid", default: 0.0
     t.float "box_cost_saved", default: 0.0
-    t.boolean "tv", default: false
-    t.boolean "call_fix_fr", default: false
-    t.boolean "call_mob_fr", default: false
+    t.boolean "tv", default: true
+    t.boolean "call_fix_fr", default: true
+    t.boolean "call_mob_fr", default: true
+    t.string "name", default: "Box Internet"
     t.bigint "full_simulation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -199,9 +200,9 @@ ActiveRecord::Schema.define(version: 2019_09_18_080120) do
     t.string "last_name"
     t.string "phone_number"
     t.boolean "is_admin", default: false
+    t.bigint "city_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "city_id"
     t.string "provider"
     t.string "uid"
     t.index ["city_id"], name: "index_users_on_city_id"
