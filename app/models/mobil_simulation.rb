1e56b4fc4cd7f56c4960096ab23b529eddf361d9
iroshiva <jean.weil@protonmail.com>
2019-09-18 10:39:01 +0200

    finish def comparison mobil simu model

--- app/models/mobil_simulation.rb
+++ app/models/mobil_simulation.rb
@@ -18,7 +18,24 @@ class MobilSimulation < ApplicationRecord
 
   # Def comparison
   def comparison(monthly_cost, bundle_go, calls_europe, calls_international, net_international)
-  	
+  	monthly_cost = monthly_cost.to_i
+  	first_filter = MobilContract.all.select { |contract| contract.bundle_gbyte >= bundle_go}
+  	second_filter = first_filter.select { |contract| contract.calls_europe == calls_europe}
+  	third_filter = second_filter.select { |contract| contract.calls_international == calls_international}
+  	fourth_filter = third_filter.select { |contract| contract.net_international == net_international}
+
+  	fifth_filter = fourth_filter.select { |contract| contract.bundle_price < monthly_cost } # filter by price
+  	max_save = 0
+    all_savings = []
+    fifth_filter.each do |contract|
+      savings = ((monthly_cost - contract.price_month) * 12 ).round(2) # Find the best price
+      if savings > max_save
+        max_save = savings # Save the best price
+      end
+      all_savings << savings # Save the differences in an array, it will be use after
+    end
+
+  	[max_save.round(2), fifth_filter, all_savings] # return the max save, all the better contracts and the savings for each contracts
   end
 
 end
