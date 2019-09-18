0b50ca8dd33858e9d6db50946913d0087f8ee63d
Giancarlo <gpbene@gmail.com>
2019-09-18 12:55:09 +0200

    begin privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/service_operation'
   get 'static_pages/legal_information'
+  get 'static_pages/privacy_policy'
 
   namespace :admin do
     root to: 'users#index'
