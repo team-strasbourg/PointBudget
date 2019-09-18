6fcf5e1f2a6f590185a09bc1956c657ca429a8a7
Carsano <damien.fabiano@gadz.org>
2019-09-18 13:25:14 +0200

    add show for full simulation in admin

--- app/controllers/admin/full_simulations_controller.rb
+++ app/controllers/admin/full_simulations_controller.rb
@@ -2,10 +2,9 @@
 
 module Admin
   class FullSimulationsController < ApplicationController
-
     def show
-      @user = User.find(params{:user_id})
       @full_simulation = FullSimulation.find(params[:id])
+      @user = @full_simulation.user
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,8 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'users#index'
-    resources :users do
-      resources :full_simulations, only: [:show]
-    end
+    resources :users
+    resources :full_simulations, only: [:show]
     resources :gas_contracts
     resources :box_contracts
     resources :ele_contracts
