a186cea98a999eb59885f6e73499c1bc32e5cb88
Carsano <damien.fabiano@gadz.org>
2019-09-19 11:02:57 +0200

    add mail for users welcome and goodbye

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   after_create :welcome_send
-  # before_destroy :goodbye_send
+  before_destroy :goodbye_send
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
